fbfc5f7874a150382acbe4afcf1a7be63b39ddb7
Lourdes Mae <enjambre.lourdes707@gmail.com>
2025-11-27 16:32:53 +0800

    firebase_file

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// FIREBASE SETUP FOR TECHNO-LUMIVANA
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVZgNyG3moBt1HaetQ8RTqCf47ROwpU1U",
+  authDomain: "lumivana-c0488.firebaseapp.com",
+  projectId: "lumivana-c0488",
+  storageBucket: "lumivana-c0488.firebasestorage.app",
+  messagingSenderId: "593132826538",
+  appId: "1:593132826538:web:28a0840c2c41d0860dc5d3",
+  measurementId: "G-FTWR6Y7LNB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Database (for commissions)
+export const db = getFirestore(app);
+
+// Storage (for images)
+export const storage = getStorage(app);
